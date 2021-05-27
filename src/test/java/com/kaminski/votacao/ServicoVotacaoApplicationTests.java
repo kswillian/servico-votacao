cba405741b096bc08d841c0638907eda386b13a4
willian-kaminski <developer.williansantos@gmail.com>
2021-05-27 08:57:36 -0300

    iniciando o projeto.

---
+++ src/test/java/com/kaminski/votacao/ServicoVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminski.votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
