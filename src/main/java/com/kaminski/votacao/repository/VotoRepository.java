232d039483ee6ff1b617641c14056639347c4106
willian-kaminski <developer.williansantos@gmail.com>
2021-05-27 15:01:11 -0300

    adicionando novos metodos para consulta dos votos.

--- src/main/java/com/kaminski/votacao/repository/VotoRepository.java
+++ src/main/java/com/kaminski/votacao/repository/VotoRepository.java
@@ -1,7 +1,17 @@
 package com.kaminski.votacao.repository;
 
+import com.kaminski.votacao.model.documents.OpcaoVoto;
 import com.kaminski.votacao.model.documents.Voto;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface VotoRepository extends MongoRepository<Voto, String> {
+
+    Boolean existsByAssociadoIdAndPautaId(String associadoId, String pautaId);
+
+    List<Voto> findAllByPautaId(String idPauta);
+
+    Integer countByPautaIdAndOpcaoVoto(String idPauta, OpcaoVoto opcaoVoto);
+
 }
