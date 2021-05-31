81daa93e3df6e7243453a97cc6814c3dadae7049
willian-kaminski <developer.williansantos@gmail.com>
2021-05-31 02:01:27 -0300

    adicionando scheduling para realizar o encerramento da sessão e envio do resultado da votação para a fila do RabbitMQ.

--- src/main/java/com/kaminski/votacao/scheduling/SessaoScheduling.java
+++ src/main/java/com/kaminski/votacao/scheduling/SessaoScheduling.java
@@ -1,10 +1,16 @@
 package com.kaminski.votacao.scheduling;
 
+import com.kaminski.votacao.model.documents.Sessao;
+import com.kaminski.votacao.model.dto.ResultadoPautaDto;
+import com.kaminski.votacao.producer.ProdutorResultadoVotacao;
 import com.kaminski.votacao.service.SessaoService;
+import com.kaminski.votacao.service.VotoService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @Slf4j
@@ -12,13 +18,22 @@ import org.springframework.stereotype.Component;
 public class SessaoScheduling {
 
     private SessaoService sessaoService;
+    private VotoService votoService;
+    private ProdutorResultadoVotacao produtorResultadoVotacao;
 
     @Scheduled(cron = "15 * * * * *")
     public void verificarSessoesEncerradasParaDivulgar(){
         log.info("Verificando sessões para divulgar...");
+
+        var sessoes = sessaoService.listarSessoesEncerradasNaoDivulgadas();
+        sessoes.forEach(sessao -> {
+            var resultadoPautaDto = votoService.buscarTotalVotosPorPauta(sessao.getPautaId());
+            sessaoService.divulgar(sessao);
+            produtorResultadoVotacao.produzirMensagem(resultadoPautaDto);
+        });
     }
 
-    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "*/1 * * * * *")
     public void verificarSessoesParaEncerrar(){
 
         log.info("Verificando sessões para encerrar...");
